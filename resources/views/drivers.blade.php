05f29a195fef22f9d46961fb63420d91159519b9
mafisz <mafisz@gmail.com>
2018-09-07 14:32:19 +0200

    driver image

--- resources/views/drivers.blade.php
+++ resources/views/drivers.blade.php
@@ -6,22 +6,24 @@
     <div class="container">
         <div class="row justify-content-center">
             @foreach($users as $user)
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="driver"> 
-                        <a href="{{ url('kierowca', $user->driver->id) }}">
-                        @if($user->driver && $user->driver->file_id)
-                            <img src="{{ url('/public/driver', $user->driver->file->path) }}" class="img-fluid">
-                        @else
-                            <img src="{{ url('/images/driver.png') }}" class="img-fluid">
-                        @endif
-                        <h6 class="my-3">
-                                @if($user->driver->show_name){{ $user->driver->name }}@endif
-                                @if($user->driver->show_lastname){{ $user->driver->lastname }}@endif
-                                @if(!$user->driver->show_lastname && !$user->driver->show_name) Anonim @endif
-                        </h6>
-                        </a>
+                @if($user->driver)
+                    <div class="col-md-6 col-lg-4 col-xl-3">
+                        <div class="driver">
+                            <a href="{{ url('kierowca', $user->driver->id) }}">
+                            @if($user->driver->file_id)
+                                <img src="{{ url('/public/driver', $user->driver->file->path) }}" class="img-fluid">
+                            @else
+                                <img src="{{ url('/images/driver.png') }}" class="img-fluid">
+                            @endif
+                            <h6 class="my-3">
+                                    @if($user->driver->show_name){{ $user->driver->name }}@endif
+                                    @if($user->driver->show_lastname){{ $user->driver->lastname }}@endif
+                                    @if(!$user->driver->show_lastname && !$user->driver->show_name) Anonim @endif
+                            </h6>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                @endif
             @endforeach
         </div>
     </div>
