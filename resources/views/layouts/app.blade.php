9c5292f014e97175cceb1db113696224d100cbb7
mafisz <mafisz@gmail.com>
2018-09-10 14:07:18 +0200

    qf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Rajdowy Puchar Śląska" />
-    <meta property="og:image" content="{{ asset('/images/logo.png') }}" />
+    <meta property="og:image" content="{{ asset('/images/header.jpg') }}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
