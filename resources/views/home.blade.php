bf68c1e9bc52e96405710b48d6b242703e9a3889
mafisz <mafisz@gmail.com>
2018-09-12 13:51:11 +0200

    news image link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,12 +94,12 @@
             <div class="col-sm-6 col-md-4 col-lg-3">
                 <div class="news"> 
                     @if($post->file_id)
-                        <img src="{{ url('public/post', $post->file->path) }}" class="img-fluid">
+                        <a href="{{ url('aktualnosc', $post->id) }}"><img src="{{ url('public/post', $post->file->path) }}" class="img-fluid"></a>
                     @endif
                     <div class="news-body">
                         <p>{!! str_limit(strip_tags($post->text, '<p><span><br /><br><strong>'), 100) !!}</p>
 
-                        <a href="{{ url('aktualnosc', $post->id) }}"><span>więcej</span></a>
+                        <a href="{{ url('aktualnosc', $post->id) }}" class="news-btn"><span>więcej</span></a>
 
                         <div class="news-date">
                             <span class="day">{{ $post->created_at->format('d') }}</span>
